a58532432155d06d1f15e842e0da6da07fec63aa
ThomasHidalgo <thomas.hidalgo@flutilliant.com>
2020-04-03 17:10:14 +0200

    Add Audio component

--- src/graphical/sdl/src/component/Audio.cpp
+++ src/graphical/sdl/src/component/Audio.cpp
@@ -13,4 +13,12 @@ using namespace component;
 
 Audio::Audio(engine::ecs::Entity& entity, const std::vector<std::string>& paths) : engine::component::AAudio(entity, paths)
 {
+    SDL_LoadWAV(paths[LIBTYPE::GRAPHIC].c_str(), &wavSpec, &wavBuffer, &wavLength);
+    deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
+}
+
+Audio::~Audio()
+{
+    SDL_CloseAudioDevice(deviceId);
+    SDL_FreeWAV(wavBuffer);
 }
